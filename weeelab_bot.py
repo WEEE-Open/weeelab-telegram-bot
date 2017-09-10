4222801af3b744220909984c055755cbf1e0d4b7
davecanni <davidecanni@hotmail.it>
2017-09-10 13:24:55 +0200

    test

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -74,9 +74,7 @@ class BotHandler:
         if not get_result:
             return -1
         elif len(get_result) > 0:  # check if there are new messages
-            print len(get_result)
-            print get_result
-            return get_result[0]  # return the last message in json format
+            return get_result[-1]  # return the last message in json format
         else:
             return -1
             # in case of error return an error code used in the main function
@@ -113,7 +111,6 @@ def main():
         time.sleep(5)
         #weee_bot.get_updates(new_offset)
         # call the function to check if there are new messages
-        new_offset = new_offset + 1
         last_update = weee_bot.get_last_update(new_offset)
         # takes the last message from the server
         # Variables for /inlab command
@@ -164,7 +161,7 @@ def main():
                                       .get_last_modified() + timedelta(hours=2)
                 last_update_id = last_update['update_id']
                 # store the id of the bot taken from the message
-                new_offset = last_update_id
+                new_offset = last_update_id + 1
                 # store the update id of the bot
                 command = last_update['message']['text'].split()
                 # store all the words in the message in an array
