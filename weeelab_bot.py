4c73d29a218dee2c466c14cbc100b7ab49833dad
davecanni <davidecanni@hotmail.it>
2017-09-10 13:34:11 +0200

    test

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -108,8 +108,7 @@ def main():
     # set at beginning an offset None for the get_updates function
 
     while True:
-        time.sleep(5)
-        #weee_bot.get_updates(new_offset)
+        weee_bot.get_updates(new_offset)
         # call the function to check if there are new messages
         last_update = weee_bot.get_last_update(new_offset)
         # takes the last message from the server
