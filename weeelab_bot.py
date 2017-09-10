72fc1eb140283743d08799570c8e028e6105adee
davecanni <davidecanni@hotmail.it>
2017-09-10 12:49:58 +0200

    test

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -109,7 +109,7 @@ def main():
     while True:
         #weee_bot.get_updates(new_offset)
         # call the function to check if there are new messages
-        last_update = weee_bot.get_last_update(new_offset+1)
+        last_update = weee_bot.get_last_update(new_offset)
         # takes the last message from the server
         # Variables for /inlab command
         user_inlab_list = ''
@@ -159,7 +159,7 @@ def main():
                                       .get_last_modified() + timedelta(hours=2)
                 last_update_id = last_update['update_id']
                 # store the id of the bot taken from the message
-                new_offset = last_update_id + 1
+                new_offset = -last_update_id - 1
                 # store the update id of the bot
                 command = last_update['message']['text'].split()
                 # store all the words in the message in an array
