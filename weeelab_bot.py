492fed8fae307d463fdf6819c321450fbf7be043
davecanni <davidecanni@hotmail.it>
2017-09-10 11:19:05 +0200

    Debug

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -494,7 +494,7 @@ After authorization /start the bot.')
                 oc.put_file_contents(
                     USER_BOT_PATH, user_bot_contents.encode('utf-8'))
                 # write on the file the new data
-
+            last_update = -1
 
 # call the main() until a keyboard interrupt is called
 if __name__ == '__main__':
