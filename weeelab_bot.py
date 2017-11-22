32b8062908705262f4ed73c627bde688b028d057
davecanni <davidecanni@hotmail.it>
2017-11-22 10:48:48 +0100

    update

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -494,8 +494,8 @@ After authorization /start the bot.')
                 # Store a new user name and id in a file on owncloud server,
                 # encoding in utf.8
                 try:
-                    user_bot_contents = user_bot_contents.decode('utf-8') \
-                                    + '\'' + last_user_name.decode('utf-8') \
+                    user_bot_contents = user_bot_contents.encode('utf-8') \
+                                    + '\'' + last_user_name.encode('utf-8') \
                                     + '\'' + ': ' + '\'' + str(last_user_id) \
                                     + '\'' + ', '
                     oc.put_file_contents(
