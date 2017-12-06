8e02ee9b71bc5b68e5a64db49761461571845f75
davecanni <davidecanni@hotmail.it>
2017-12-06 11:34:25 +0100

    Update weeelab_bot.py

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -496,10 +496,10 @@ After authorization /start the bot.')
                 # encoding in utf.8
                 try:
                     user_bot_contents = user_bot_contents.decode('utf-8') \
-                                    + str("' " + last_user_name).decode('utf-8') \
-                                    + str("' : ").decode('utf-8') \
-                                    + str("' " + last_user_id).decode('utf-8') \
-                                    + str("' , ").decode('utf-8')
+                                    + str("' " + last_user_name).encode('utf-8') \
+                                    + str("' : ").encode('utf-8') \
+                                    + str("' " + last_user_id).encode('utf-8') \
+                                    + str("' , ").encode('utf-8')
                     oc.put_file_contents(
                         USER_BOT_PATH, user_bot_contents.encode('utf-8'))
                 # write on the file the new data
