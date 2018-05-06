76082289a0e1f17e503ad5c57e7ccbd95a00d736
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-05-06 18:27:49 +0200

    Less debugging, doesn't work, it will never work

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -300,9 +300,8 @@ lab right now:\n{}'.format(people_inlab, user_inlab_list))
                                         else:
                                             msg += 'Unknown change {}'.format(change)
                                         entries += 1
-                                        msg += '{} by _{}_\n\n'.format(h_time, h_user)
+                                        msg += '{} by {}\n\n'.format(h_time, h_user)
                                         if entries >= 4:
-                                            print msg
                                             weee_bot.send_message(last_chat_id, msg)
                                             msg = ''
                                             entries = 0
