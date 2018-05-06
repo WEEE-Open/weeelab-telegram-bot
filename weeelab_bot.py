6cf7b52ba170fc1dd29dd1da73d93d161899e8ea
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-05-06 18:22:24 +0200

    More debugging

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -300,7 +300,7 @@ lab right now:\n{}'.format(people_inlab, user_inlab_list))
                                         else:
                                             msg += 'Unknown change {}'.format(change)
                                         entries += 1
-                                        msg += '{} by {}\n\n'.format(h_time, h_user)
+                                        msg += '{} by _{}_\n\n'.format(h_time, h_user)
                                         if entries >= 4:
                                             print msg
                                             weee_bot.send_message(last_chat_id, msg)
