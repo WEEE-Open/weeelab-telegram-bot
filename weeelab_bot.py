ffcda21d00838097a3c3a54850dd5e45f6a14366
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-04-17 10:51:07 +0200

    Alternate escape

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -112,7 +112,7 @@ class BotHandler:
 
 
 def escape_all(string):
-    return string.replace('_', '\\_').replace('*', '\\*').replace('`', '\\``').replace('[', '\\[').replace('<', '\\<').replace('>', '\\>')
+    return string.replace('<', '&lt;').replace('>', '&gt;')
 
 
 class CommandHandler:
