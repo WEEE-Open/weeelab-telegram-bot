b5d0b7f245d340c0c93458eeddd1e5db0c29a187
e-caste-mbp <en.cas.dev@icloud.com>
2019-11-20 14:13:26 +0100

    add volume for enhanced logic

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -758,16 +758,18 @@ as well.\nFor a list of the available commands type /help.', )
 
         elif query == AcceptableQueriesLoFi.volume_down:
             # os.system("amixer -c 0 set PCM 3dB-")  # system volume
-            if lofi_player.audio_set_volume(lofi_player.audio_get_volume() - 10) == 0:
-                self.__edit_message(messge_id, "Volume down 10% - current volume: " + str(lofi_player.audio_get_volume()), self.lofi_keyboard(playing))
+            volume = lofi_player.audio_get_volume()
+            if lofi_player.audio_set_volume(volume - 10) == 0:
+                self.__edit_message(messge_id, "Volume down 10% - current volume: " + str(volume-10), self.lofi_keyboard(playing))
             else:  # == -1
                 self.__edit_message(messge_id, "The volume is already muted.", self.lofi_keyboard(playing))
 
         elif query == AcceptableQueriesLoFi.volume_plus:
             # os.system("amixer -c 0 set PCM 3dB+")  # system volume
-            if lofi_player.audio_get_volume() <= 100:
-                if lofi_player.audio_set_volume(lofi_player.audio_get_volume() + 10) == 0:
-                    self.__edit_message(messge_id, "Volume up 10% - current volume: " + str(lofi_player.audio_get_volume()), self.lofi_keyboard(playing))
+            volume = lofi_player.audio_get_volume()
+            if volume <= 100:
+                if lofi_player.audio_set_volume(volume + 10) == 0:
+                    self.__edit_message(messge_id, "Volume up 10% - current volume: " + str(volume+10), self.lofi_keyboard(playing))
                 else:
                     self.__edit_message(messge_id, "There was an error pumpin' up. Lame.", self.lofi_keyboard(playing))
             else:  # == -1
