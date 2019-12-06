7906b78777138dcc4b62014a194caa2e8292bf32
e-caste-mbp <en.cas.dev@icloud.com>
2019-12-06 20:21:57 +0100

    add debug lines

--- ssh_util.py
+++ ssh_util.py
@@ -49,6 +49,8 @@ class SSHUtil:
         if self.commands is None:
             print("WARNING: No commands given.", file=stderr)
         print(isinstance(self.commands, str))
+        print(type(self.commands))
+        print(self.commands)
         if isinstance(self.commands, str):
             self.commands = [self.commands]  # make iterable list from single command
             print(self.commands)
