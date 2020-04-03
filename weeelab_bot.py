a58d2c062b8651727f588fc36bf86c5b9c69f197
e-caste-mbp <en.cas.dev@icloud.com>
2020-04-03 10:57:08 +0200

    add missing newline

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -231,7 +231,7 @@ def fah_ranker(bot: BotHandler, hour: int, minute: int):
                                 f"<i>{member['wus']}</i> WUs%s"
                                 % f"""{f", rank <i>{member['rank']}</i>" if 'rank' in member else ""}"""
                                 for member in json_res['donors'][:40]])
-            text = f"Total Team Score: <b>{json_res['credit']}</b>" \
+            text = f"Total Team Score: <b>{json_res['credit']}</b>\n" \
                    f"Total Team Work Units: <b>{json_res['wus']}</b>\n" \
                    f"Team Rank: {json_res['rank']}/{json_res['total_teams']} " \
                    f"-> top <b>{round(json_res['rank']/json_res['total_teams']*100, 2)}%</b>\n" \
