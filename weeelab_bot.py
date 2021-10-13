ac7951e33e5388a9c8abd10e7c34616132b27b95
ThePalm99 <ThePalm99@users.noreply.github.com>
2021-10-13 19:42:12 +0200

    Fix.

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -1190,7 +1190,7 @@ as well.\nFor a list of the available commands type /help.', )
                     self.bot.active_sessions[idx][2] = data[1]
                     return
             print(f"Saved date in active_session: {data[1]}")
-            self.bot.active_sessions[idx][2] = data[1]
+            self.bot.active_sessions[0][2] = data[1]
         for idx, session in enumerate(self.bot.active_sessions):
             if user_id == session[0]:
                 return
