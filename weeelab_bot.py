26b726058cc8e69b20bd1cffa13b76b4ef31989a
ThePalm99 <ThePalm99@users.noreply.github.com>
2021-10-14 11:08:20 +0200

    Fix.

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -727,11 +727,9 @@ as well.\nFor a list of the available commands type /help.', )
         day = date.split()
         day[1] = datetime.datetime.strptime(day[1], "%B").month
         day = f'{day[0]} {day[1]} {day[2]}'
-        print(f"day_funct: {day}")
         day = datetime.datetime.strptime(day, "%d %m %Y")
         today = datetime.datetime.now().timetuple()
         today = f"{today.tm_mday} {today.tm_mon} {today.tm_year}"
-        print(f"today: {today}")
         today = datetime.datetime.strptime(today, "%d %m %Y")
         diff = day - today
         return diff.days
@@ -1180,8 +1178,6 @@ as well.\nFor a list of the available commands type /help.', )
             for idx, session in enumerate(self.bot.active_sessions):
                 if session[0] == user_id:
                     day = self._get_tolab_gui_days(idx, self.bot.active_sessions[idx][2])
-                    print(f"day: {day}")
-                    print(f"data: {data}")
                     if day < 0:
                         self.bot.edit_message(chat_id=self.__last_chat_id, message_id=message_id,
                                               text="❌ You've selected a past date. Please select a valid date.")
@@ -1852,8 +1848,6 @@ def main():
                     flag = True
                     user_id = last_update['message']['from']['id']
                     tolab_active_sessions = handler.get_tolab_active_sessions()
-                    print(f"active_sessions: {tolab_active_sessions}")
-                    print(f"user_id: {last_update['message']['from']['id']}")
                     for idx, session in enumerate(tolab_active_sessions):
                         if user_id in session:
                             handler.tolab_callback(f"hour:{command[0]}", session[1], user_id)
