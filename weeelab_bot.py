45c6f4ec1b957d3c7cedd77097c34be2b5b39eef
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2021-10-22 21:15:30 +0200

    Upcoming safety tests reminder is working again, close #108
    Thanks @alecello asd

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -1495,7 +1495,7 @@ as well.\nFor a list of the available commands type /help.', )
 
                 test_people = []
                 for p in self.people.get_all(self.conn):
-                    if p.accountlocked and p.dateofsafetytest and p.dateofsafetytest == datetime.date.today():
+                    if p.dateofsafetytest and p.dateofsafetytest == datetime.date.today():
                         if p.tgid:
                             test_people.append(CommandHandler.__get_telegram_link_to_person(p))
                         else:
