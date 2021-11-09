d3d78975b04dfe6f0c4398401d103d64a06d0813
il-palmi <39865402+il-palmi@users.noreply.github.com>
2021-11-10 00:51:53 +0100

    Fixed time input in tolab command (#112)
    Added '/tolab no' and '/inlab' info on tolab confirm message and sir reminder

--- weeelab_bot.py
+++ weeelab_bot.py
@@ -1181,12 +1181,19 @@ as well.\nFor a list of the available commands type /help.', )
             for idx, session in enumerate(self.bot.active_sessions):
                 if session[0] == user_id:
                     day = self._get_tolab_gui_days(idx, self.bot.active_sessions[idx][2])
-                    sir_message = ""
-                    if len(data[-1]) > 2 or len(data[-1]) < 2 or len(data[-2]) > 2 or len(data[-2]) < 2:
-                        self.bot.edit_message(chat_id=self.__last_chat_id, message_id=message_id,
-                                              text="❌ Use correct time format, e.g. 10:30. Please, retry /tolab")
-                        del self.bot.active_sessions[idx]
-                        return
+                    sir_message = ''
+                    if data[-2] != 'hour':
+                        if len(data[-1]) > 2 or len(data[-1]) < 2 or len(data[-2]) > 2 or len(data[-2]) < 2:
+                            self.bot.edit_message(chat_id=self.__last_chat_id, message_id=message_id,
+                                                  text="❌ Use correct time format, e.g. 10:30. Please, retry /tolab")
+                            del self.bot.active_sessions[idx]
+                            return
+                    else:
+                        if len(data[-1]) > 2 or len(data[-1]) < 2:
+                            self.bot.edit_message(chat_id=self.__last_chat_id, message_id=message_id,
+                                                  text="❌ Use correct time format, e.g. 10:30. Please, retry /tolab")
+                            del self.bot.active_sessions[idx]
+                            return
                     if (not self.user.signedsir) and (self.user.dateofsafetytest is not None):
                         sir_message = "\nRemember to sign the SIR when you get there! 📝"
                         # if people do tolab for a day that is after tomorrow then send also the "mark it down" message
